f628ba0832dd145af034875163182b9084ab7a75
Nicholas Kajoh <kajohterna@gmail.com>
2019-03-18 13:16:24 +0100

    Add vehicle counter

--- Vehicle_Counting.py
+++ Vehicle_Counting.py
@@ -1,5 +1,5 @@
 import cv2
-from blob2 import Blob, get_centroid, box_contains_point
+from blob2 import Blob, get_centroid, box_contains_point, get_area
 import numpy as np
 from collections import OrderedDict
 from yolo_detector import get_bounding_boxes
@@ -9,8 +9,8 @@ cap = cv2.VideoCapture('./video_data/sample_traffic_scene.mp4')
 blobs = OrderedDict()
 blob_id = 1
 frame_counter = 0
-DETECTION_FRAME_RATE = 10
-MAX_CONSECUTIVE_TRACKING_FAILURES = 15
+DETECTION_FRAME_RATE = 5
+MAX_CONSECUTIVE_TRACKING_FAILURES = 10
 
 # initialize trackers and create new blobs
 _, frame = cap.read()
@@ -21,12 +21,17 @@ for box in initial_bboxes:
     _blob = Blob(box, tracker)
     blobs[blob_id] = _blob
 
+f_height, f_width, _ = frame.shape
+cl_y = round(4 / 5 * f_height)
+counting_line = [(0, cl_y), (f_width, cl_y)]
+vehicle_count = 0
+
 while True:
     if cap.get(cv2.CAP_PROP_POS_FRAMES) + 1 < cap.get(cv2.CAP_PROP_FRAME_COUNT):
         _, frame = cap.read()
         
-        # update trackers
         for _id, blob in list(blobs.items()):
+            # update trackers
             success, box = blob.tracker.update(frame)
             if success:
                 blob.num_consecutive_tracking_failures = 0
@@ -42,18 +47,23 @@ while True:
             if blob.num_consecutive_tracking_failures >= MAX_CONSECUTIVE_TRACKING_FAILURES:
                 del blobs[_id]
 
-        # rerun detection, add new blobs 
+            # count vehicles
+            if blob.centroid[1] >= cl_y and not blob.counted:
+                blob.counted = True
+                vehicle_count += 1
+
         if frame_counter >= DETECTION_FRAME_RATE:
+            # rerun detection
             boxes = get_bounding_boxes(frame)
             
+            # add new blobs
             for box in boxes:
                 box_centroid = get_centroid(box)
-                box_area = box[2] * box[3] # w * h
+                box_area = get_area(box)
                 match_found = False
                 for _id, blob in blobs.items():
-                    blob_area = blob.bounding_box[2] * blob.bounding_box[3]
-                    if blob_area >= box_area and box_contains_point(blob.bounding_box, box_centroid) \
-                            or box_area >= blob_area and box_contains_point(box, blob.centroid):
+                    if (blob.area >= box_area and box_contains_point(blob.bounding_box, box_centroid)) \
+                            or (box_area >= blob.area and box_contains_point(box, blob.centroid)):
                         match_found = True
                         tracker = cv2.TrackerKCF_create()
                         tracker.init(frame, tuple(box))
@@ -66,8 +76,25 @@ while True:
                     tracker.init(frame, tuple(box))
                     _blob = Blob(box, tracker)
                     blobs[blob_id] = _blob
+
+            # remove duplicate blobs
+            for id_a, blob_a in list(blobs.items()):
+                for id_b, blob_b in list(blobs.items()):
+                    if blob_a == blob_b:
+                        break
+
+                    if blob_a.area >= blob_b.area and box_contains_point(blob_a.bounding_box, blob_b.centroid):
+                        del blobs[id_b]
+                    elif blob_b.area >= blob_a.area and box_contains_point(blob_b.bounding_box, blob_a.centroid):
+                        del blobs[id_a]
             
             frame_counter = 0
+
+        # draw counting line
+        cv2.line(frame, counting_line[0], counting_line[1], (0, 255, 0), 3)
+
+        # display vehicle count
+        cv2.putText(frame, 'Count: ' + str(vehicle_count), (20, 60), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2, cv2.LINE_AA)
 
         resized_frame = cv2.resize(frame, (858, 480))
         cv2.imshow('tracking', resized_frame)
