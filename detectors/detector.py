af060c156cd7e67fa871182473fd69abea7e6c31
Nicholas Kajoh <kajohterna@gmail.com>
2019-05-10 08:53:52 +0100

    Add SSD as a vehicle detector

--- detectors/detector.py
+++ detectors/detector.py
@@ -1,6 +1,7 @@
 from detectors.yolo.yolo_detector import get_bounding_boxes as yolo_gbb
 from detectors.haarc.hc_detector import get_bounding_boxes as hc_gbb
 from detectors.bgsub.bgsub_detector import get_bounding_boxes as bgsub_gbb
+from detectors.ssd.ssd import get_bounding_boxes as ssd_gbb
 
 def get_bounding_boxes(frame, model):
     if model == 'yolo':
@@ -9,5 +10,7 @@ def get_bounding_boxes(frame, model):
         return hc_gbb(frame)
     elif model == 'bgsub':
         return bgsub_gbb(frame)
+    elif model == 'ssd':
+        return ssd_gbb(frame)
     else:
-        raise Exception('Invalid detector model/algorithm specified (options: yolo, haarc, bgsub)')
+        raise Exception('Invalid detector model/algorithm specified (options: yolo, haarc, bgsub, ssd)')
