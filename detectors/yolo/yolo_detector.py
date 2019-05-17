9f3efeb6895dfe6139ca8c3ddef9f8b84df20725
Yemi Kudaisi <yemikudaisi@gmail.com>
2019-05-17 22:23:56 +0100

    added tests for yolo detector config

--- detectors/yolo/yolo_detector.py
+++ detectors/yolo/yolo_detector.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import os
-from configparser import SafeConfigParser
+from configparser import ConfigParser
 
 class VehicleCountingConfigNotFound(Exception):
     pass
@@ -22,6 +22,7 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 VC_CONFIG_YOLO_SECTION = 'yolo'
 VC_CONFIG_YOLO_WEIGHTS_OPTION = 'weights'
 VC_CONFIG_YOLO_CONFIG_OPTION = 'yolov3.cfg'
+DEFAULT_WEIGHTS_FILE = 'yolov3.weights'
 def get_bounding_boxes(image):
     # get object classes
     classes = None
@@ -31,16 +32,16 @@ def get_bounding_boxes(image):
     
     # create a YOLO v3 DNN model using pre-trained weights
     net = None
-    weights_path = os.path.join(__location__, 'yolov3.weights')
-    yolo_cfg_path  = os.path.join(__location__, 'yolov3.cfg')
+    weights_file = os.path.join(__location__, DEFAULT_WEIGHTS_FILE)
+    yolo_cfg_file  = os.path.join(__location__, 'yolov3.cfg')
 
-    is_weights_exists = os.path.isfile(weights_path)
-    is_cfg_exists = os.path.isfile(yolo_cfg_path)
+    is_weights_exists = os.path.isfile(weights_file)
+    is_cfg_exists = os.path.isfile(yolo_cfg_file)
     
-    config_parser = SafeConfigParser()
+    config_parser = ConfigParser()
     # if either the weight or cfg is absent 
     if not is_weights_exists:  
-        error_message = 'Unable to locate yolo.weights in default location\n'
+        error_message = '\nUnable to locate yolo.weights in default location\n'
         weight_found = False              
         
         # check for a config file in the current working directory
@@ -48,38 +49,36 @@ def get_bounding_boxes(image):
 
         #if vehicle counting config  file exists
         if(os.path.isfile(vc_config)):
+            config_parser.read(vc_config)
             #check for yolo weights config secton
             if(config_parser.has_section(VC_CONFIG_YOLO_SECTION)):
                 #check for weights options in section
                 if(config_parser.has_option(VC_CONFIG_YOLO_SECTION, VC_CONFIG_YOLO_WEIGHTS_OPTION)):
-                    #if the file in weights option exists
+                    #check if the file in weights option exists
                     if(os.path.isfile(config_parser.get(VC_CONFIG_YOLO_SECTION,VC_CONFIG_YOLO_WEIGHTS_OPTION))):
-                        weights_path = config_parser.get(VC_CONFIG_YOLO_SECTION,VC_CONFIG_YOLO_WEIGHTS_OPTION)
+                        weights_file = config_parser.get(VC_CONFIG_YOLO_SECTION,VC_CONFIG_YOLO_WEIGHTS_OPTION)
                         weight_found = True
                     else:
                         error_message += 'The file specified in the yolo weight option of the Vehicle-Counting config does not exist\n'
                         raise WeightsConfigOptionFileNotFoundError(error_message)
                 else:
                     error_message += 'A weights option was not found in yolo section of the Vehicle-Counting config file\n'  
-                    raise WeightsConfigOptionFileNotFoundError(error_message)
+                    raise YoloConfigSectionNotFoundError(error_message)
             else:
                 error_message += 'A yolo section was not found in the Vehicle-Counting config file\n'
                 raise YoloConfigSectionNotFoundError(error_message)
         else: # config file 
             error_message+='Vehicle Counting config file not found\n'
             raise VehicleCountingConfigNotFound(error_message)
-        
 
-    if not is_cfg_exists:
-        print('Unable to locate yolo config, please specify path in vc.cfg')
+    # make sure yolo confi file exists
+    if(not is_cfg_exists):
+        exit("yolo config file not found")
 
     try:
-        net = cv2.dnn.readNet(weights_path, yolo_cfg_path)
+        net = cv2.dnn.readNet(weights_file, yolo_cfg_file)
     except:
-        print("Unable to create yolo DDN model")
-        exit()
-
-    
+        exit("\n\nUnable to create yolo DDN model")
 
     # create image blob
     scale = 0.00392
