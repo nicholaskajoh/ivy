56b4ba574081a510f0eca03a216e4b2ef58dade2
Nicholas Kajoh <kajohterna@gmail.com>
2019-09-22 21:33:27 +0100

    Setup linting with pylint

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
-import cv2
+'''
+VCS entry point.
+'''
+
+import ast
 import os
+
+import cv2
+from dotenv import load_dotenv
+
 from VehicleCounter import VehicleCounter
-import numpy as np
-import uuid
-import contextlib
-import argparse
 from util.logger import log_info, log_error
 from util.image import take_screenshot
-from dotenv import load_dotenv
-import ast
 
 
 def run():
+    '''
+    Loads environment variables, initializes counter class and runs counting loop.
+    '''
     # capture traffic scene video
     is_cam = ast.literal_eval(os.getenv('IS_CAM'))
     video = int(os.getenv('VIDEO')) if is_cam else os.getenv('VIDEO')
     cap = cv2.VideoCapture(video)
     if not cap.isOpened():
-        log_error('Error capturing video. Invalid source.', { 'cat': 'VIDEO_CAPTURE', 'source': video })
+        log_error('Error capturing video. Invalid source.', {'cat': 'VIDEO_CAPTURE', 'source': video})
     ret, frame = cap.read()
     f_height, f_width, _ = frame.shape
 
-    di = int(os.getenv('DI'))
+    detection_interval = int(os.getenv('DI'))
     mcdf = int(os.getenv('MCDF'))
     mctf = int(os.getenv('MCTF'))
     detector = os.getenv('DETECTOR')
     tracker = os.getenv('TRACKER')
     # create detection region of interest polygon
     use_droi = ast.literal_eval(os.getenv('USE_DROI'))
-    droi = ast.literal_eval(os.getenv('DROI')) if use_droi else [(0, 0), (f_width, 0), (f_width, f_height), (0, f_height)]
+    droi = ast.literal_eval(os.getenv('DROI')) \
+            if use_droi \
+            else [(0, 0), (f_width, 0), (f_width, f_height), (0, f_height)]
     show_droi = ast.literal_eval(os.getenv('SHOW_DROI'))
     counting_line_position = os.getenv('COUNTING_LINE_POSITION')
 
-    vehicle_counter = VehicleCounter(frame, detector, tracker, droi, show_droi, mcdf, mctf, di, counting_line_position)
+    vehicle_counter = VehicleCounter(frame, detector, tracker, droi, show_droi, mcdf,
+                                     mctf, detection_interval, counting_line_position)
 
     record = ast.literal_eval(os.getenv('RECORD'))
     headless = ast.literal_eval(os.getenv('HEADLESS'))
@@ -44,11 +52,11 @@ def run():
                                         30, \
                                         (f_width, f_height))
 
-    log_info('Processing started...', {
-            'cat': 'COUNT_PROCESS',
-            'counter_config': { 'di': di, 'mcdf': mcdf, 'mctf': mctf, 'detector': detector,
-                                'tracker': tracker, 'use_droi': use_droi, 'droi': droi,
-                                'show_droi': show_droi, 'clp': counting_line_position }})
+    log_info('Processing started...',
+             {'cat': 'COUNT_PROCESS',
+              'counter_config': {'di': detection_interval, 'mcdf': mcdf, 'mctf': mctf, 'detector': detector,
+                                 'tracker': tracker, 'use_droi': use_droi, 'droi': droi, 'show_droi': show_droi,
+                                 'clp': counting_line_position}})
     # main loop
     while is_cam or cap.get(cv2.CAP_PROP_POS_FRAMES) + 1 < cap.get(cv2.CAP_PROP_FRAME_COUNT):
         if ret:
@@ -67,9 +75,9 @@ def run():
         if k == ord('s'): # save frame if 's' key is pressed
             take_screenshot(output_frame)
         if k == ord('q'): # end video loop if 'q' key is pressed
-            log_info('Processing stopped.', { 'cat': 'COUNT_PROCESS' })
+            log_info('Processing stopped.', {'cat': 'COUNT_PROCESS'})
             break
-        
+
         ret, frame = cap.read()
 
     # end capture, close window, close log file and video object if any
@@ -78,7 +86,7 @@ def run():
         cv2.destroyAllWindows()
     if record:
         output_video.release()
-    log_info('Processing ended.', { 'cat': 'COUNT_PROCESS' })
+    log_info('Processing ended.', {'cat': 'COUNT_PROCESS'})
 
 if __name__ == '__main__':
     load_dotenv()
